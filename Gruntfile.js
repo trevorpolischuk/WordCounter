60b14d7904cd884fbe832b9504717e5addd7537f
Trevor Polischuk <trevorpolischuk@gmail.com>
2013-12-08 01:26:01 -0800

    Added minified version and uglify plugin for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,42 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),	
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     qunit: {
-      files: ['index.html']
+        files: ['index.html']
     },
     jshint: {
-      files: ['Gruntfile.js', 'js/main.js', 'tests/resources/tests.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
+        files: ['Gruntfile.js', 'js/main.js', 'tests/resources/tests.js'],
+        options: {
+            // options here to override JSHint defaults
+            globals: {
+                jQuery: true,
+                console: true,
+                module: true,
+                document: true
+            }
         }
-      }
+    },
+    // Project configuration.
+    uglify: {
+        my_target: {
+            files: {
+                'js/wordcounter.min.js': ['js/main.js']
+            }
+        }
     },
     watch: {
-      files: ['js/main.js'],
-      tasks: ['jshint', 'qunit']
+        files: ['js/main.js'],
+        tasks: ['jshint', 'qunit', 'uglify']
     }
-  });
+});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-qunit');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['qunit','jshint', 'watch']);
+grunt.registerTask('default', ['qunit','jshint', 'watch', 'uglify']);
 
 };
